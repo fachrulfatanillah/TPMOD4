2a4717cba3661796970f92e27ca75b4a02f07da8
fachrulfatanillah <fachrul.fatanillah@gmail.com>
2022-03-18 15:33:30 +0800

    menambahkan class DataGeneric

---
+++ src/pkg1302204009_tpmod4_fachrur/data.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1302204009_tpmod4_fachrur;
+
+/**
+ *
+ * @author dedyh
+ */
+public class data {
+       public String dat;
+    public data(String x){
+        this.dat = x;
+    }
+}
